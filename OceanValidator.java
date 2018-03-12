f76bb4da7506e9232c84b5f05137d5efac263ec3
WojTechM <wojciech.m1997@gmail.com>
2018-03-12 14:18:42 +0100

    Add sample verion of 'validateOcean' function in OceanValidator class

--- OceanValidator.java
+++ OceanValidator.java
@@ -3,7 +3,22 @@ public class OceanValidator {
 
     public static boolean validateOcean(String[][] ocean, Ship ship) {
 
-        
+        boolean valid = true;
+
+        boolean vertical = ship.getVertical();
+        for (int i = 0; i < ship.getShipSize(); i++) {
+
+            int x = getXOfSquare(i);
+            int y = getYOfSquare(i);
+
+            if (vertical) {
+                valid = checkVerticalSquare(ocean, x, y, i);
+            } else {
+                valid = checkHorizontalSquare
+            }
+        }
+
+        return valid;
 
     }
 
