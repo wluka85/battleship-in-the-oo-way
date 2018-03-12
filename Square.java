a215aa9a3520883f76425e4ad05a45cc0aa3965e
wluka85 <wluka85@gmail.com>
2018-03-12 13:14:55 +0100

    Initialize Square class with constructor and getters

---
+++ Square.java
@@ -0,0 +1,20 @@
+public class Square {
+    private int x;
+    private int y;
+
+
+    public Square(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public int getIndexX() {
+        return x;
+    }
+
+
+    public int getIndexY() {
+        return y;
+    }
+}
