06a858958c9dc441874000d264571dca02eb244f
WojTechM <wojciech.m1997@gmail.com>
2018-03-13 10:58:51 +0100

    Implement methods required in AI class

--- AI.java
+++ AI.java
@@ -1,6 +1,19 @@
 
 public class AI {
 
-    
+    private String[][] enemyOcean = new board[10][10];
+    public Ocean ocean;
 
+    public AI (Ocean ocean) {
+        this.ocean = ocean;
+        addShipsToOcean();
+    }
+    
+    private void addShipsToOcean() {
+        Ship carrier = new Ship();
+    }
+
+    public void takeATurn() {
+
+    }
 }
