8f04de24f2b1199c85f5ce8f218151365cd1f901
WojTechM <wojciech.m1997@gmail.com>
2018-03-13 12:37:30 +0100

    Implemented sample verion of constructor in Player class

--- Player.java
+++ Player.java
@@ -1,6 +1,11 @@
 
 public class Player {
 
+    private static final int OCEAN_SIZE = 10;
+    private String[][] enemyOcean = new String[10][10];
+    public Ocean ocean;
 
-
+    public Player(Ocean ocean) {
+        this.ocean = Ocean;
+    }
 }
