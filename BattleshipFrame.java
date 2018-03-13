0c00bcd89f96bb6e7606fb7a5343115e54325140
wluka85 <wluka85@gmail.com>
2018-03-13 13:50:04 +0100

    Implement interface MouseListener and add methods mouseClicked, mouseEntered, mouseExited, mousePressed and MouseReleased in BattleshipFrame class

--- BattleshipFrame.java
+++ BattleshipFrame.java
@@ -1,8 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 
-public class BattleshipFrame extends JFrame {
+public class BattleshipFrame extends JFrame implements MouseListener {
     private static final int DEFAULT_WIDTH = 1020;
     private static final int DEFAULT_HEIGHT = 500;
     private JLabel[][] squaresLabelMy = new JLabel[10][10];
@@ -24,6 +25,7 @@ public class BattleshipFrame extends JFrame {
         for (int i = 0; i < squaresLabel.length; i++) {
             for (int j = 0; j < squaresLabel[i].length; j++) {
                 squaresLabel[i][j] = getSquareLabel(squaresLabel[i][j]);
+                squaresLabel[i][j].addMouseListener(this);
                 oceanPanel.add(squaresLabel[i][j]);
             }
         }
@@ -45,5 +47,38 @@ public class BattleshipFrame extends JFrame {
 
     public Dimension getPrefferedSize() {
       return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+
+    public void mouseClicked(MouseEvent e) {
+        Object source = e.getSource();
+        for (int i = 0; i < squaresLabelMy.length; i++) {
+            for (int j = 0; j < squaresLabelMy[i].length; j++) {
+                if(source == squaresLabelMy[i][j]) {
+
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+
+     @Override
+     public void mouseReleased(MouseEvent e) {
     }
 }
