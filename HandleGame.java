6a7f4281c4e30f4b7540ee5f31a8033dcf79867e
WojTechM <wojciech.m1997@gmail.com>
2018-03-13 12:30:40 +0100

    Implement sample version of HandleGame methods with basic imports and variables

--- HandleGame.java
+++ HandleGame.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.lang.Object;
 
 public class HandleGame {
 
+    private List<Object> players = new ArrayList<>();
+    private List<Ocean> oceans = new ArrayList<>();
 
+    public void pvp() {
 
+    }
+
+    public void pvAI() {
+
+    }
+
+    public void aivAI() {
+
+    }
+
+    public String takeAShot(int x, int y, Ocean ocean) {
+
+    }
 }
