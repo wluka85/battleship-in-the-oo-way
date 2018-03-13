5a8f725a1fe55e9d0f8c8eb8386806b272045454
WojTechM <wojciech.m1997@gmail.com>
2018-03-13 12:47:06 +0100

    Implemented functions creating 'players'

--- HandleGame.java
+++ HandleGame.java
@@ -7,15 +7,27 @@ public class HandleGame {
     private List<Ocean> oceans = new ArrayList<>();
 
     public void pvp() {
+        oceans.add(new Ocean());
+        players.add(new Player(oceans.get(0)));
 
+        oceans.add(new Ocean());
+        players.add(new Player(oceans.get(1)));
     }
 
     public void pvAI() {
+        oceans.add(new Ocean());
+        players.add(new Player(oceans.get(0)));
 
+        oceans.add(new Ocean());
+        players.add(new AI(oceans.get(1)));
     }
 
     public void aivAI() {
+        oceans.add(new Ocean());
+        players.add(new AI(oceans.get(0)));
 
+        oceans.add(new Ocean());
+        players.add(new AI(oceans.get(1)));
     }
 
     public String takeAShot(int x, int y, Ocean ocean) {
