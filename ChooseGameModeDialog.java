03f21e90d26b43bd43f24405f1f8b41b522ff693
wluka85 <wluka85@gmail.com>
2018-03-14 14:15:42 +0100

    Add buttonPanel with buttons and getGameMode in ChooseGameModeDialog class

--- ChooseGameModeDialog.java
+++ ChooseGameModeDialog.java
@@ -3,20 +3,60 @@ import javax.swing.*;
 import java.awt.event.*;
 
 
-public class ChooseGameModeDialog {
-    private static final int DEFAULT_WIDTH = 200;
-    private static final int DEFAULT_HEIGHT = 400;
+public class ChooseGameModeDialog extends JDialog{
+    private static final int DEFAULT_WIDTH = 180;
+    private static final int DEFAULT_HEIGHT = 150;
+    private JPanel buttonPanel;
+    private JLabel informationLabel;
+    private JButton pvAIButton, pvpButton, aivAIButton;
+    private String gameMode = "aivAI";
 
 
 
     public ChooseGameModeDialog(JFrame owner) {
         super(owner, "Choose game mode", true);
+        setLayout(new BorderLayout());
+        informationLabel = new JLabel("Choose game mode: ");
+        add(informationLabel, BorderLayout.NORTH);
+        buttonPanel = new JPanel();
+        pvAIButton = new JButton("Player vs AI");
+        pvpButton = new JButton("Player vs Player");
+        aivAIButton = new JButton("Simulation");
+        pvAIButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                gameMode = "pvAI";
+                dispose();
+            }
+        });
+        pvpButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                gameMode = "pvp";
+                dispose();
+            }
+        });
+        aivAIButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                gameMode = "aivAI";
+                dispose();
+            }
+        });
+        buttonPanel.add(pvAIButton);
+        buttonPanel.add(pvpButton);
+        buttonPanel.add(aivAIButton);
+        add(buttonPanel, BorderLayout.CENTER);
+        setSize(getPrefferedSize());
+        setVisible(true);
     }
 
 
 
     public Dimension getPrefferedSize() {
         return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+
+    public String getGameMode() {
+        return gameMode;
     }
 
 }
