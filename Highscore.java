dc243dd900515986cb25b6e2114cf7602c624f66
WojTechM <wojciech.m1997@gmail.com>
2018-03-14 12:05:22 +0100

    Implement getHighscore method

--- Highscore.java
+++ Highscore.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -5,13 +8,40 @@ import java.util.List;
 public class Highscore {
 
     public void manageHighscore(String playersName, int points) {
-        
+
         List<String> highscores = new ArrayList<String>();
         highscores = getHighscores();
         highscores = addCurrentScore(playersName, points);
         saveTop10Scores(highscores);
-    
+
     }
 
-    
+    private List<String> getHighscores() {
+        List<String> highscores = new ArrayList<String>();
+        File file = new File("scores.csv");
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String text = null;
+
+            while ((text = reader.readLine()) != null) {
+                list.add(text);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return highscores;
+    }
 }
