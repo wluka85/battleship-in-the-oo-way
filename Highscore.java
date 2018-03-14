178af06ef95b0b07498ac3356da00bd6924412e1
WojTechM <wojciech.m1997@gmail.com>
2018-03-14 11:49:45 +0100

    Implement sample version of manageHighscore method

--- Highscore.java
+++ Highscore.java
@@ -1,6 +1,12 @@
-
+import java.time.LocalDateTime;
 public class Highscore {
 
-
+    public void manageHighscore(String playersName, int points) {
+        
+        String[] highscores = getHighscores();
+        highscores = addCurrentScore(playersName, points);
+        saveTop10Scores(highscores);
     
+    }
+
 }
