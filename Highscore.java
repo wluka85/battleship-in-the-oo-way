a10bc4e28b441f0a0f68cf2d97d65437531db5cf
WojTechM <wojciech.m1997@gmail.com>
2018-03-14 11:52:56 +0100

    Update imports, and change in variable 'highscores' type

--- Highscore.java
+++ Highscore.java
@@ -1,12 +1,17 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Highscore {
 
     public void manageHighscore(String playersName, int points) {
         
-        String[] highscores = getHighscores();
+        List<String> highscores = new ArrayList<String>();
+        highscores = getHighscores();
         highscores = addCurrentScore(playersName, points);
         saveTop10Scores(highscores);
     
     }
 
+    
 }
